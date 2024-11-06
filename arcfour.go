95939e79d432abf978dce580a71c0f256b27e8c1
sidgupt12 <sidgupt12@gmail.com>
2024-11-07 00:48:55 +0530

    made template for writing encryption code

---
+++ arcfour.go
@@ -0,0 +1,11 @@
+/* arcfour.go */
+package main
+
+func main() {
+
+	var key, from, encrypted, decrypted string
+
+	key = "tomatoes" // can be 8 bits to 2048 bits
+	from = "Shall I compare thee to a summer's day?"
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sidgupt12/arcfour_encryption_library
+
+go 1.23.0
