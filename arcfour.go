36cd6b05dc4f87ac7d01020c4df7a07b5aa73e07
sidgupt12 <sidgupt12@gmail.com>
2024-11-07 22:00:46 +0530

    wrote logic for initializing the arcfour cipher

--- arcfour.go
+++ arcfour.go
@@ -13,19 +13,45 @@ func assert(cond bool, msg string) {
 }
 
 type Arcfour struct {
-	// ...
+	s       [256]int
+	i, j, k int
 }
 
-func rc4init(key string, keylen int) Arcfour {
-	// ...
+func rc4init(key string, size int) Arcfour {
+	var p Arcfour
+	for x := 0; x < 256; x++ {
+		p.s[x] = 0
+	}
+	p.i, p.j, p.k = 0, 0, 0
+
+	var temp1, temp2 int
+
+	for p.i = 0; p.i < 256; p.i++ {
+		p.s[p.i] = p.i
+	}
+
+	for p.i = 0; p.i < 256; p.i++ {
+		temp1 = p.i % size
+		temp2 = p.j + p.s[p.i] + int(key[temp1])
+		p.j = temp2 % 256
+		p.s[p.i], p.s[p.j] = p.s[p.j], p.s[p.i]
+	}
+	p.i, p.j = 0, 0
+
+	return p
+
 }
 
 func rc4byte() int {
-
+	return 0
 }
 
 func rc4encrypt(key string, keylen int) string {
+	return ""
+}
 
+func rc4decrypt(key string, keylen int) string {
+	return rc4encrypt(key, keylen)
 }
 
 func printbin(input string, size int) {
@@ -33,7 +59,7 @@ func printbin(input string, size int) {
 	assert(size > 0, "size <= 0")
 
 	for i := 0; i < size; i++ {
-		if (i+1)%2 == 0 {
+		if i%2 == 0 {
 			fmt.Printf(" ")
 		}
 		fmt.Printf("%02x", input[i])
@@ -46,12 +72,13 @@ func main() {
 	//Arcfour * rc4
 
 	var encrypted, decrypted string
+	fmt.Println(encrypted, decrypted)
 
 	key := "tomatoes" // can be 8 bits to 2048 bits
 	from := "Shall I compare thee to a summer's day?"
 
 	skey := len(key)
-	stext := len(from)
+	//stext := len(from)
 
 	fmt.Println("Initializing encryption...")
 	os.Stdout.Sync()
